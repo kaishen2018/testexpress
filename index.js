d0781b680c7bea5e611ec01ce431fb913cd98839
yangguoyk2015 <ywkmc@163.com>
2016-09-28 16:38:34 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-var heapdump = require('heapdump');
-var express = require('express');
-var router = express.Router();
-var app = express();
+var heapdump = require('heapdump')
+var express = require('express')
+var router = express.Router()
+var app = express()
+
 var theThing = null;
-var replaceThing = function() {
-    console.log('in replaceThing function ')
-    var origninThing = theThing;
-    var unused = function(){
-        if(origninThing) {
-            console.log('hi');
-        }
-    };
-    theThing = {
-        longStr: new Array(1000000).join('*'),
-        someMethod: function() {
-            console.log('someMessage ')
-        }
-    };
+var replaceThing = function () {
+  console.log('in replaceThing function ');
+  var origninThing = theThing;
+  var unused = function () {
+    if (origninThing) {
+      console.log('hi');
+    }
+  };
+  theThing = {
+    longStr: new Array(1000000).join('*'),
+    someMethod: function () {
+      console.log('someMessage ');
+    }
+  };
 };
-router.get('/leak', function(req, res, next) {
-    replaceThing();
-    theThing = null;
-    heapdump.writeSnapshot('testexpress' + Date.now() + '.heapsnapshot');
-    return res.json({message: 'Everything is fine'})
+router.get('/leak', function (req, res, next) {
+  replaceThing();
+  theThing = null;
+  heapdump.writeSnapshot('testexpress' + Date.now() + '.heapsnapshot');
+  return res.json({message: 'Everything is fine'});
 });
+
+router.get('/leak2', function (req, res, next) {
+  return res.json({message: 'Everything is fine'});
+});
+
 app.get('/*', router);
-app.listen(3000, function(err){
-    console.log('express listening on 3000');
+app.listen(3000, function (err) {
+  if (err) {
+    console.log('error occurs');
+  }
+  console.log('express listening on 3000');
 });
 exports.default = app;
